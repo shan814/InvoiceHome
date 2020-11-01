a34142e16213cdedb67496f1eb7f8df2a6599dbe
Muhammad Shan Aslam <shanaslamkhan@gmail.com>
2020-11-01 11:52:32 -0800

    new header style adjusted

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -6,53 +6,60 @@ import { Dropdown, MenuItem, DropdownButton } from "react-bootstrap";
 export default function Header1() {
   return (
     <div class="navbar">
-      <div class="container flex">
-        <h1 class="logo">Logo</h1>
-        <nav>
-          <ul>
-            <li>
-              {/* <a href="index.html">Home</a> */}
-              <Link to={"/sign-in"}>
-                <button
-                  style={{ fontSize: 15 }}
-                  type="button"
-                  //   class="btn btn-primary"
-                  className="btn mr-2 btn-warning invoiceButton"
-                >
-                  Sign In
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Dropdown>
-                <Dropdown.Toggle
-                  variant="success"
-                  id="dropdown-basic"
-                  className=" btn-primary"
-                >
-                  Language and Settings
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="/">Arabic</Dropdown.Item>
-                  <Dropdown.Item href="/">English</Dropdown.Item>
-                  <Dropdown.Item href="/change-password">
-                    Change Password
-                  </Dropdown.Item>
-                  <Dropdown.Item href="/email-reports">
-                    Email Reports
-                  </Dropdown.Item>
-                  <Dropdown.Item href="/language-region">
-                    Language and Region
-                  </Dropdown.Item>
-                  <Dropdown.Item href="/name-email">
-                    My Name And Email
-                  </Dropdown.Item>
-                  <Dropdown.Item href="/">Sign Out</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </li>
-          </ul>
-        </nav>
+      <div class="container-fluid">
+        <div className="row class-row">
+          <div className="col-md-3 class-logo">
+            <h1 class="logo">Logo</h1>
+          </div>
+
+          <div className="col-md-3 class-right">
+            <nav>
+              <ul>
+                <li>
+                  {/* <a href="index.html">Home</a> */}
+                  <Link to={"/sign-in"}>
+                    <button
+                      style={{ fontSize: 15 }}
+                      type="button"
+                      //   class="btn btn-primary"
+                      className="btn mr-2 btn-warning invoiceButton"
+                    >
+                      Sign In
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      variant="success"
+                      id="dropdown-basic"
+                      className=" btn-primary class-dropdown"
+                    >
+                      Language and Settings
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="/">Arabic</Dropdown.Item>
+                      <Dropdown.Item href="/">English</Dropdown.Item>
+                      <Dropdown.Item href="/change-password">
+                        Change Password
+                      </Dropdown.Item>
+                      <Dropdown.Item href="/email-reports">
+                        Email Reports
+                      </Dropdown.Item>
+                      <Dropdown.Item href="/language-region">
+                        Language and Region
+                      </Dropdown.Item>
+                      <Dropdown.Item href="/name-email">
+                        My Name And Email
+                      </Dropdown.Item>
+                      <Dropdown.Item href="/">Sign Out</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </div>
   );
